01ecb09d62b12141608f3cf22bda61bd33c49180
Pakizamutallim <62bq5tn@code.edu.az>
2024-09-09 16:13:14 +0400

    added

--- challenge/4-delete_dnodeint/free_dlistint.c
+++ challenge/4-delete_dnodeint/free_dlistint.c
@@ -1,27 +1,18 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the prev node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * for Holberton project
+ * free_dlistint - Frees a doubly linked list
+ * @head: The pointer to the head of the doubly linked list
  */
-typedef struct dlistint_s
+void free_dlistint(dlistint_t *head)
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
-} dlistint_t;
+    dlistint_t *tmp;
 
-size_t print_dlistint(const dlistint_t *h);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-void free_dlistint(dlistint_t *head);
-
-#endif
+    while (head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
